6a6566ca4a304e5579a0c466e77e36ca660f9c88
kevin <kevin.sennoun@gmail.com>
2025-01-06 19:05:18 +0100

    fix header nav

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,20 @@
 'use client';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
 
 const Header = () => {
-    const { logout } = useContext(AuthContext);
+    const { logout, profile } = useContext(AuthContext);
+    const router = useRouter();
+    const pathname = usePathname();
+    useEffect(() => {
+        console.log('Header rendered');
+    }, [router]);
     return (
-        <header>
+        <header className='flex space-x-4 pr-4 pt-4 mb-4 justify-end'>
+            {profile?.role === 'admin' && pathname !== '/admin' && <Link href="/admin">Admin</Link>}
+            {pathname !== '/' && <Link href="/">Accueil</Link>}
             <a onClick={logout} style={{ cursor: 'pointer' }}>Se déconnecter</a>
         </header>
     );
